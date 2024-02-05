5e31390e9fa10f580f98be30d7ac6e66ae5045f4
Leochukwu <chukwu17mc@gmail.com>
2024-02-05 10:15:12 -1100

    style and adding other functions

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
+import React, { useState } from 'react'
+import data from './data'
+import { FaQuoteRight } from 'react-icons/fa'
+import { FiChevronsRight, FiChevronsLeft } from 'react-icons/fi'
+
 function App() {
+  const [people, SetPeople] = useState(data)
+  const [index, setIndex] = useState(0)
   return (
-    <div className='App'>
-      <h1>this is it</h1>
-    </div>
+    <section className='section'>
+      <div className='title'>
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+      <div className='section-centre'>
+        {people.map((person, personIndex) => {
+          const { id, image, name, title, quote } = person
+
+          //more to come
+
+          return (
+            <article key={id}>
+              <img src={image} alt={name} className='person-img' />
+              <h4>{name}</h4>
+              <p className='title'>{title}</p>
+              <p className='text'>{quote}</p>
+              <FaQuoteRight className='icon' />
+            </article>
+          )
+        })}
+        <button className='prev'>
+          <FiChevronsLeft />
+        </button>
+        <button className='next'>
+          <FiChevronsRight />
+        </button>
+      </div>
+    </section>
   )
 }
 
